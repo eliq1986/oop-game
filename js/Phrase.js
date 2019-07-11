08d0c2e6f00396e8f1df0730aaa119c89c56989a
Elijah Quesada <this_boy10@yahoo.com>
2019-07-11 12:08:38 -0700

    Reformatted code layout with js.beautify

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,60 +1,59 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+class Phrase {
+    constructor(phrase) {
+        this.phrase = phrase;
+    }
 
-  class Phrase {
-   constructor(phrase) {
-     this.phrase = phrase;
-   }
-
-// method takes no arg; appends letters to DOM
-   addPhraseToDisplay() {
-     const phraseSplitted = this.phrase.split("");
-       phraseSplitted.forEach(letter => {
-       this.createAndAppendElement(letter);
-     });
-   }
-
-
-// method takes 1 arg event.target; checks if phrase contains letter selected
-   checkLetter(letterToCheck) {
-     const phrase = this.activePhrase.toLowerCase().split("");
-     const lettersFound = phrase.filter(letter => letter.toLowerCase() === letterToCheck.textContent);
-     if (!lettersFound.length) {
-       letterToCheck.classList.add("wrong");
-       return false;
-     } else {
-       letterToCheck.classList.add("chosen");
-       this.showMatchedLetter(letterToCheck.textContent);
-       return true;
-     }
-
-   }
+    // method takes no arg; appends letters to DOM
+    addPhraseToDisplay() {
+        const phraseSplitted = this.phrase.split("");
+        phraseSplitted.forEach(letter => {
+            this.createAndAppendElement(letter);
+        });
+    }
 
 
-// method takes 1 arg a str; takes phrase and appends each letter/space to DOM
-   createAndAppendElement(letter) {
-     const phraseContainer = document.querySelector("div#phrase ul");
-     const li = document.createElement("li");
-     if(letter !== " ") {
-       li.classList.add("letter", "hide");
-       li.textContent = letter;
-       li.setAttribute("letter", letter.toLowerCase());
-       phraseContainer.appendChild(li);
-     } else {
-       li.classList.add("space", "hide");
-       li.textContent = letter;
-       phraseContainer.appendChild(li);
-     }
+    // method takes 1 arg event.target; checks if phrase contains letter selected
+    checkLetter(letterToCheck) {
+        const phrase = this.activePhrase.toLowerCase().split("");
+        const lettersFound = phrase.filter(letter => letter.toLowerCase() === letterToCheck.textContent);
+        if (!lettersFound.length) {
+            letterToCheck.classList.add("wrong");
+            return false;
+        } else {
+            letterToCheck.classList.add("chosen");
+            this.showMatchedLetter(letterToCheck.textContent);
+            return true;
+        }
 
-   }
+    }
 
 
-// method takes 1 arg; runs if match letter and changes className => show
-     showMatchedLetter(letterToCheck) {
-       document.querySelectorAll(`li[letter=${letterToCheck}]`).
-       forEach(letter => letter.className = "show");
-     }
+    // method takes 1 arg a str; takes phrase and appends each letter/space to DOM
+    createAndAppendElement(letter) {
+        const phraseContainer = document.querySelector("div#phrase ul");
+        const li = document.createElement("li");
+        if (letter !== " ") {
+            li.classList.add("letter", "hide");
+            li.textContent = letter;
+            li.setAttribute("letter", letter.toLowerCase());
+            phraseContainer.appendChild(li);
+        } else {
+            li.classList.add("space", "hide");
+            li.textContent = letter;
+            phraseContainer.appendChild(li);
+        }
+
+    }
 
 
-  }
+    // method takes 1 arg; runs if match letter and changes className => show
+    showMatchedLetter(letterToCheck) {
+        document.querySelectorAll(`li[letter=${letterToCheck}]`).
+        forEach(letter => letter.className = "show");
+    }
+
+
+}
--- js/app.js
+++ js/app.js
@@ -1,27 +1,25 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
-
-
- const game = new Game();
+const game = new Game();
 
 // listens to start button
-document.querySelector("button#btn__reset").addEventListener("click", ()=> {
+document.querySelector("button#btn__reset").addEventListener("click", () => {
     game.startGame();
 });
 
 // listens to buttons "clicked" on only
 document.querySelector("div#qwerty").addEventListener("click", event => {
-   event.target.tagName === "BUTTON" ? game.handleInteraction(event.target) : null;
+    event.target.tagName === "BUTTON" ? game.handleInteraction(event.target) : null;
 });
 
 // listens to physical keyup on keyboard
 document.addEventListener("keyup", event => {
-const keyboardRegrex = /^[A-Za-z]$/;
-const startScreen = document.querySelector("div#overlay");
+    const keyboardRegrex = /^[A-Za-z]$/;
+    const startScreen = document.querySelector("div#overlay");
 
-  if(keyboardRegrex.test(event.key) && startScreen.style.display == "none") {
-      game.handleInteraction(event.key.toLowerCase());
-  }
+    if (keyboardRegrex.test(event.key) && startScreen.style.display == "none") {
+        game.handleInteraction(event.key.toLowerCase());
+    }
 
 });
